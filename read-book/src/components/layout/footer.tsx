c66bee0ffc21c989c1081363af65b2160635f133
Binh <binhduc0510@gmail.com>
2024-01-13 13:36:29 +0700

    add footer

--- read-book/src/components/layout/footer.tsx
+++ read-book/src/components/layout/footer.tsx
@@ -1,9 +1,28 @@
 const Footer = () => {
     return (
-            <div className="h-24">
-<h1>H</h1>
+        <div className="w-full px-0 border-t p-4 bg-no-repeat bg-cover bg-center bg-[url('https://www.pngitem.com/pimgs/m/160-1605161_cities-background-for-footer-skyline-hd-png-download.png')]">
+            <div className="container my-8 px-3">
+                <div className="py-1 my-2 flex justify-center">
+                    <img src="https://metruyencv.com/assets/images/logo.png?40124" width={64} height={64} />
+                </div>
+                <div className="w-auto text-center lg:px-36 md:px-36">
+                    Mê Truyện Chữ là nền tảng mở trực tuyến, miễn phí đọc truyện chữ được convert hoặc dịch kỹ lưỡng, do các converter và dịch giả đóng góp, rất nhiều truyện hay và nổi bật được cập nhật nhanh nhất với đủ các thể loại tiên hiệp, kiếm hiệp, huyền ảo ...
+                </div>
+                <div className="flex justify-center gap-4 my-8">
+                    <a href="#"><img src="https://metruyencv.info/assets/images/app-store.png?40124" className="w-24" /></a>
+                    <a href="#"><img src="https://metruyencv.info/assets/images/google-play.png?40124" className="w-24" /></a>
+                </div>
+                
+                <ul className="sm:flex justify-center list-none">
+                    <li><a className="sm:w-3/12 px-3 py-2 hover:text-yellow-600" href="#">Điều khoản dịch vụ</a></li>
+                    <li><a className="sm:w-3/12 px-3 py-2 hover:text-yellow-600" href="#">Chính sách bảo mật</a></li>
+                    <li><a className="sm:w-3/12 px-3 py-2 hover:text-yellow-600" href="#">Về bản quyền</a></li>
+                    <li><a className="sm:w-3/12 px-3 py-2 hover:text-yellow-600" href="#">Hướng dẫn sử dụng</a></li>
+                </ul>
+                
             </div>
-        
+        </div>
+
     )
 }
 
