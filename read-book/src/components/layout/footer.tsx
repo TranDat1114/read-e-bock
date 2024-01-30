e5e92d7d99ba8092bf3a874ad085987422d1a668
Gia Bao <nguyenhiengiabao12@gmail.com>
2024-01-30 15:19:43 +0700

    Add Link import and alt attributes to footer images

--- read-book/src/components/layout/footer.tsx
+++ read-book/src/components/layout/footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (
         <div className="w-full px-0 border-t p-4 bg-no-repeat bg-cover bg-center bg-[url('https://www.pngitem.com/pimgs/m/160-1605161_cities-background-for-footer-skyline-hd-png-download.png')]">
@@ -9,12 +11,12 @@ const Footer = () => {
                     Mê Truyện Chữ là nền tảng mở trực tuyến, miễn phí đọc truyện chữ được convert hoặc dịch kỹ lưỡng, do các converter và dịch giả đóng góp, rất nhiều truyện hay và nổi bật được cập nhật nhanh nhất với đủ các thể loại tiên hiệp, kiếm hiệp, huyền ảo ...
                 </div>
                 <div className="flex justify-center gap-4 my-8">
-                    <a href="#"><img src="https://metruyencv.info/assets/images/app-store.png?40124" className="w-24" /></a>
-                    <a href="#"><img src="https://metruyencv.info/assets/images/google-play.png?40124" className="w-24" /></a>
+                    <a href="#"><img src="https://metruyencv.info/assets/images/app-store.png?40124" alt="App Store" className="w-24" /></a>
+                    <a href="#"><img src="https://metruyencv.info/assets/images/google-play.png?40124" alt="Google Play" className="w-24" /></a>
                 </div>
                 <div className="flex justify-center">
                 <ul className="sm:flex list-none">
-                    <li className="mb-1"><a className="sm:w-3/12 px-3  hover:text-yellow-600" href="#">Điều khoản dịch vụ</a></li>
+                    <li className="mb-1"><Link className="sm:w-3/12 px-3  hover:text-yellow-600" to={"/dieu-khoan"}>Điều khoản dịch vụ</Link></li>
                     <li className="mb-1"><a className="sm:w-3/12 px-3  hover:text-yellow-600" href="#">Chính sách bảo mật</a></li>
                     <li className="mb-1"><a className="sm:w-3/12 px-3  hover:text-yellow-600" href="#">Về bản quyền</a></li>
                     <li className="mb-1"><a className="sm:w-3/12 px-3  hover:text-yellow-600" href="#">Hướng dẫn sử dụng</a></li>
